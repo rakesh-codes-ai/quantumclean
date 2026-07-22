41b55ab3a48b996615d5345c55d975c9c36d0b7e
Rakesh Alagappan Manimaran <r_alagappanmanimaran@fanshaweonline.ca>
2026-07-21 20:09:21 -0400

    Add import from contract module to init.py
    Import QualitySLA, SLABreach, and SLAReport from contract module.

--- quantumclean/__init__.py
+++ quantumclean/__init__.py
@@ -1,4 +1,4 @@
 """Quality SLA enforcement for QuantumClean."""
 from .sla import QualitySLA, SLABreach, SLAReport
-
+from .contract import QualitySLA, SLABreach, SLAReport
 __all__ = ["QualitySLA", "SLABreach", "SLAReport"]
